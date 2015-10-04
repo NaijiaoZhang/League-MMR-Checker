15acb2599aa52588fa4a6f2c9a4f9593ed94c77f
Mastapan <jerry.qy.pan@gmail.com>
2015-10-03 23:20:37 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 package com.company;
+import java.util.List;
 import java.util.Map;
+
+import dto.Match.MatchDetail;
+import dto.Match.Participant;
+import dto.Match.ParticipantIdentity;
+import dto.MatchList.MatchList;
+import dto.MatchList.MatchReference;
 import dto.Summoner.Summoner;import constant.Region;
 
 import main.java.riotapi.RiotApi;
@@ -16,7 +23,38 @@ public class Main{
         Map<String, Summoner> summoners = api.getSummonersByName(Region.NA, "summonername");
         Summoner summoner = summoners.get("summonername");
         long id = summoner.getId();
+        List<MatchReference> matchinfo = api.getMatchList(id).getMatches();
+        Long[] matchidlist = new Long[matchinfo.size()];
+        for(int i=0; i<matchinfo.size(); i ++){
+           matchidlist[i] =  matchinfo.get(i).getMatchId();
+
+        }
+
+
+
+
+
         System.out.println(id);
     }
+    public long[][] playeridlist(Long[] matchid, RiotApi api,long id) throws RiotApiException{
+        long[][] playeridlist = new long[matchid.length][9];
+        for (int i = 0; i < matchid.length; i++) {
+            MatchDetail match = api.getMatch(matchid[i]);
+            List<ParticipantIdentity> hi = match.getParticipantIdentities();
 
+            for(int j = 0 ; j < hi.size(); j ++) {
+                if (!(hi.get(j).getPlayer().getSummonerId() == id)) {
+                    playeridlist[i][j] = hi.get(j).getPlayer().getSummonerId();
+                }
+            }
+
+
+        }
+        return playeridlist;
+
+    }
+    public int[][] elochart(int[][] playeridlist){
+
+
+    }
 }
